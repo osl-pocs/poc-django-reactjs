fadb950ce255f49ec2cbdab8826291f94fc8deb5
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2024-10-03 12:49:46 -0400

    initial commit

---
+++ src/backend/mhai_web/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from poc_django_reactjs.users.models import User
+from poc_django_reactjs.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
